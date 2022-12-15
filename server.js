578c076dd5c04b0148f496892b38587fa5ea7a42
MoBebba123 <mohamedbebba1@gmail.com>
2022-12-15 22:37:04 +0100

    upload image

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express  from "express";
 import multer from "multer";
+import cors from "cors";
 
 const upload =  multer({dest:"uploads/"})
 
 const app =  express();
 const PORT = 8000;
 
-
+app.use(cors());
+app.post("/images",upload.single("image"),(req, res,next)=>{
+    const file = req.file;
+    const description = req.body.description;
+    console.log(file)
+    res.send("done")
+})
 
 app.listen(PORT, ()=>{ console.log("app is running on Port " + PORT ) })
